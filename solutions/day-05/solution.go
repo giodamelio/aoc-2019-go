3eb11e45f13a65b43817385b9b3d938117038c9f
Gio d'Amelio <giodamelio@gmail.com>
2021-04-26 16:25:04 +0000

    Solve day 5 part 1

---
+++ solutions/day-05/solution.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	_ "embed"
+	"log"
+	"strconv"
+	"strings"
+
+	"github.com/giodamelio/aoc-2020-go/intcode"
+)
+
+// Read the raw input
+//go:embed input.txt
+var rawInput string
+
+func parseInput(input string) ([]int, error) {
+	bytes := strings.Split(strings.TrimSpace(input), ",")
+	numbers := make([]int, len(bytes))
+
+	for index, programByte := range bytes {
+		number, err := strconv.Atoi(programByte)
+		if err != nil {
+			return nil, err
+		}
+		numbers[index] = number
+	}
+
+	return numbers, nil
+}
+
+func part1(input []int) []int {
+	log.Println("Day 5 Part 1")
+
+	computer := intcode.NewComputer(input)
+
+	// Select Air Conditioning Unit
+	computer.SendInput(1)
+
+	// Listen for outputs and when they are done send them on a channel
+	outputChan := computer.GetOutputChannel()
+	allOutputs := make(chan []int)
+	go func() {
+		var outputs []int
+		for i := range outputChan {
+			outputs = append(outputs, i)
+		}
+
+		allOutputs <- outputs
+	}()
+
+	computer.Run()
+
+	return <-allOutputs
+}
+
+func part2(input []int) int {
+	log.Println("Day 5 Part 2")
+
+	return 0
+}
---
+++ solutions/day-05/solution_test.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"os"
+	"testing"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"github.com/stretchr/testify/assert"
+)
+
+func init() {
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr, NoColor: true})
+	zerolog.SetGlobalLevel(zerolog.TraceLevel)
+}
+
+func TestParseInput(t *testing.T) {
+	parsedInput, err := parseInput("1,2,3")
+
+	assert.Nil(t, err, "Parsing returned an error")
+	assert.Equal(t, []int{1, 2, 3}, parsedInput)
+}
+
+func TestParseInputNonInteger(t *testing.T) {
+	parsedInput, err := parseInput("1,haha,3")
+
+	assert.Error(t, err)
+	assert.Equal(t, err.Error(), "strconv.Atoi: parsing \"haha\": invalid syntax")
+	assert.Nil(t, parsedInput)
+}
+
+func TestPart1(t *testing.T) {
+	parsedInput, err := parseInput(rawInput)
+	assert.Nil(t, err)
+
+	output := part1(parsedInput)
+	allButLast := output[:len(output)-1]
+	last := output[len(output)-1]
+
+	// Ensure the last output is the solution
+	assert.Equal(t, 4511442, last)
+
+	// Ensure that all the preceding outputs are zero
+	for _, outputElement := range allButLast {
+		assert.Equal(t, 0, outputElement)
+	}
+}
+
+func TestPart2(t *testing.T) {
+	parsedInput, err := parseInput(rawInput)
+	assert.Nil(t, err)
+
+	assert.Equal(t, 0, part2(parsedInput))
+}
